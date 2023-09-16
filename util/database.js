94ecbfcecfef53aba48c3d146fc9a25e65b94f48
unknown <harinirswekrish@gmail.com>
2023-09-16 18:29:07 +0530

    Done changes

--- util/database.js
+++ util/database.js
@@ -1,7 +1,7 @@
 
 const Sequelize=require('sequelize');
 
-const sequelize = new Sequelize('test', 'root', 'root', {
+const sequelize = new Sequelize('test', 'root', '123456', {
   host: 'localhost',
   dialect: 'mysql'
 });
